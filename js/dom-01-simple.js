0f73c1727093c0bd790c6287bc285dc6e064bffa
Monsser <gogyandtom@gmail.com>
2026-01-18 21:54:23 +0700

    Updates

--- js/dom-01-simple.js
+++ js/dom-01-simple.js
@@ -1,12 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // ดึงข้อมูลจาก input ที่มี class app-inp-number
   const inpNumber = [...document.querySelectorAll(".app-inp-number")];
+  // ทำการวนลูปเพื่อเพิ่ม event listener ให้กับแต่ละ input
   inpNumber.forEach((elem) =>
     elem.addEventListener("change", () => {
+      // คำนวนผลรวมของ input
       const result = inpNumber.reduce(
         (result, elem) => result + (Number.isNaN(elem.valueAsNumber) ? 0 : elem.valueAsNumber),
         0,
       );
       const outpNumber = document.querySelectorAll(".app-outp-number");
+      // ทำการวนลูปเพื่อเพิ่ม event listener ให้กับแต่ละ output
       outpNumber.forEach((elem) => (elem.textContent = result));
     }),
   );
